15903c7823818d772de9e5303284ea1a14f02ad7
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-11-19 19:13:17 +0100

    Configuring angular and ceating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoItemComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule
